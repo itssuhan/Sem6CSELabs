483fd3842a742473993f549a35a3e728d04528c1
Suhan Ashraf <92210776+itssuhan@users.noreply.github.com>
2024-01-22 17:27:04 +0530

    q3 completed.

--- DSLab/Lab 3 - Python Basics - III/L3Q3.py
+++ DSLab/Lab 3 - Python Basics - III/L3Q3.py
@@ -1,9 +1,4 @@
 # Operations on Arrays (use numpy wherever required): 
- 
-# e) Reshape 3X4 array to 2X2X3 array 
-# f) Find maximum and minimum element of array, Row wise max and min, column wise max 
-# and min and sum of elements. (Use functions max(), min(), sum())
-
 
 import numpy as np
 
@@ -26,7 +21,16 @@ print(C)
 
 # d) Create a sequence of integers  from 0 to 20 with steps of 5
 
+D = np.arange(0, 20, 5)
+print(D)
 
+# e) Reshape 3X4 array to 2X2X3 array
 
+E = np.reshape(C, (2, 2, 3))
+print(E)
 
+# f) Find maximum and minimum element of array, Row wise max and min, column wise max 
+# and min and sum of elements. (Use functions max(), min(), sum())
 
+F = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(np.min(F, axis=1), np.min(F, 0), np.max(F, 1), np.max(F, 0), np.sum(F, 1), np.sum(F, 0))
