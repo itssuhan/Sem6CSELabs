a7cb0ebc18eb76450e970b1625011853f0c10161
Suhan Ashraf <92210776+itssuhan@users.noreply.github.com>
2024-01-22 17:13:28 +0530

    ds files added.

---
+++ DSLab/Lab 1 - Python Basics - I/l1q2.py
@@ -0,0 +1,14 @@
+#Swap Values of Two Variable
+
+a=int(input("Enter the Value for a "))
+b=int(input("Enter the value for b "))
+
+c = a
+a = b
+b = c
+
+print("Value of a is", a)
+print("Value of b is", b)
+
+
+
---
+++ DSLab/Lab 1 - Python Basics - I/l1q3.py
@@ -0,0 +1,11 @@
+#Even or Odd
+
+a = int(input("Enter the value of a "))
+
+if(a%2==0):
+    print(a, "is even")
+else:
+    print(a ,"is odd")
+        
+
+
