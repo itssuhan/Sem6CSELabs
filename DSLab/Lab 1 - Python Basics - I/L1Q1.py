abc3ab350646441f48624a261b8a43afe4c1f0ff
Suhan Ashraf <92210776+itssuhan@users.noreply.github.com>
2024-01-22 17:12:50 +0530

    ds lab files added.

---
+++ DSLab/Lab 1 - Python Basics - I/L1Q1.py
@@ -0,0 +1,8 @@
+# Area of Rectangle
+
+a = int(input("Enter width a of the rectangle"))
+b = int(input("Enter length b of the rectangle"))
+
+#Calculating Area of the Rectangle
+c = a*b
+print("The Area of the Rectangle is", c,"cm\u00b2")
